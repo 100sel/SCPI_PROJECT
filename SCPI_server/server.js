33adcf92fb245fb495e1d807f14fed08ac740bd5
100sel <romain.peturaud@gmail.com>
2023-02-14 21:07:28 +0100

    MIRCI YOYO

--- SCPI_server/server.js
+++ SCPI_server/server.js
@@ -3,36 +3,25 @@ const app = express()
 const fs = require('fs');
 const { GetData } = require('./getData')
 
-function Refresh() {
+async function refresh() {
+    console.log("Reading json");
+    let jsonData = await fs.promises.readFile('../data.json', 'utf-8');
+    let finalData;
 
-    async function Process() {
-        console.log("Reading json");
-        let jsonData = await fs.promises.readFile('../data.json', 'utf-8');       
-        let finalData;
-
-        if (jsonData.length < 3) {
-            console.log("Getting data");
-            let crawlerData = await GetData();
-            fs.writeFile('../data.json', JSON.stringify(crawlerData), () => console.log("JSON written"));
-            finalData = crawlerData;
-        } else {
-            finalData = await JSON.parse(jsonData);
-        }
-
-        return finalData
+    if (jsonData.length < 3) {
+        console.log("Getting data");
+        let crawlerData = await GetData();
+        fs.writeFile('../data.json', JSON.stringify(crawlerData), () => console.log("JSON written"));
+        finalData = crawlerData;
+    } else {
+        finalData = await JSON.parse(jsonData);
     }
-
-    const Main = new Promise((res) => {    
-        let Data = Process();       
-        res(Data); 
-    });
-
-    return Main;
+    return finalData;
 }
 
 app.get("/api/refresh", (req, res) => {
     console.log("Refreshing data");
-    Refresh().then(data => res.json({ scpiList : data }));
+    refresh().then(data => res.json({ scpiList : data }));
 })
 
 app.listen(5000, () => { console.log("Server started on port 5000") });
